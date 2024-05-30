d1d2933d1d88d50b5c0316d078176ec5520ae8ad
Najrul <mdnajrulislam.4738@gmail.com>
2024-05-30 17:35:24 +0600

    set counting method for product quantity.

--- js/product-details.js
+++ js/product-details.js
@@ -1,3 +1,5 @@
+/*
+javascript for swiper
 var swiper = new Swiper(".swiper-container", {
   // Optional parameters
   direction: "horizontal",
@@ -24,4 +26,24 @@ var swiper = new Swiper(".swiper-container", {
   // scrollbar: {
   //   el: ".swiper-scrollbar",
   // },
+});
+
+*/
+
+// Javascript for product quantity.
+let quantityElement = document.getElementById("quantity");
+let quantity = parseInt(quantityElement.innerText);
+
+// for increment quantity.
+document.getElementById("increase-btn").addEventListener("click", function () {
+  quantity += 1;
+  quantityElement.textContent = quantity;
+});
+
+// for decrement quantity.
+document.getElementById("decrease-btn").addEventListener("click", function () {
+  if (quantity > 0) {
+    quantity -= 1;
+  }
+  quantityElement.textContent = quantity;
 });
